4525a11f803715519cbe4a58a44a967c171ccb6e
Eddy G <eddy.spambari@gmail.com>
2023-05-14 22:33:15 -0700

    token and DEX setup

---
+++ contracts/DEX.sol
@@ -0,0 +1,28 @@
+pragma solidity >= 0.4.22 <=0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DEX {
+    IERC20 public associatedToken;
+
+    uint price;
+    address owner;
+
+    constructor(IERC20 _token, uint _price) {
+        associatedToken = _token;
+        owner = msg.sender;
+        price = _price;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "you are not the owner");
+        _;
+    }
+
+    function sell() external onlyOwner {
+        uint allowance = associatedToken.allowance(msg.sender, address(this));
+        require(allowance > 0, "you must allow this contract access to at least one token");
+        bool sent = associatedToken.transferFrom(msg.sender, address(this), allowance);
+        require(sent, "failed to send");
+    }
+}
--- contracts/token.sol
+++ contracts/token.sol
@@ -2,6 +2,8 @@ pragma solidity >= 0.4.22 <=0.8.19;
 
 import "@openzeppelin/contract/token/ERC20/ERC20.sol";
 
-contract Token is ERC20 {
-    
+contract Token is ERC20 { //ERC20 takes name and symbol in constructor
+    constructor(uint initialSupply) ERC20("SwizzCoin", "SWZ") {
+        _mint(msg.sender, initialSupply); //sends initialSupply of tokens to deployer of contract. _mint is internal only
+    }
 }
